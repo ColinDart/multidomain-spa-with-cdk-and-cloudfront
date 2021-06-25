f4d143f1adec96c4c94bbf724fa08721a1d4ff9f
Colin Dart <41739180+ColinDart@users.noreply.github.com>
2021-06-25 23:38:21 +1000

    chore: fix typo in comment

--- bin/cdk-wildCardStaticApp.ts
+++ bin/cdk-wildCardStaticApp.ts
@@ -5,7 +5,7 @@ import { wildCardStaticApp } from '../lib/wildCardStaticApp-stack';
 const app = new cdk.App();
 const envUS  = { 
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'us-east-1' // Note the region is hard coded to us-east-1 as this is the requiref region for CloudFront certificates
+    region: 'us-east-1' // Note the region is hard coded to us-east-1 as this is the required region for CloudFront certificates
 };
 
 new wildCardStaticApp(app, 'wildCardStaticApp', { env: envUS} );
